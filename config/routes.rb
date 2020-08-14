69ac6f49e39b6e4fa62d9742b6287a39500715d6
Ari Drookman <aga.drookman@gmail.com>
2020-08-13 21:13:40 -0400

    Some temporary changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => "welcome#index"
   # get '/destination/get_coordinates/:name', to: 'destination#coordinates'
-  resources :destination do
-    collection do
-      get 'get_coordinates/:name', to: 'destination#get_coordinates'
+  devise_for :users, :controllers => {registrations: 'users/registrations', sessions: 'users/sessions'}
+  resource :users do
+    resources :trips do
+      resources :destinations do
+        collection do
+          get 'get_coordinates/:name', to: 'destination#get_coordinates'
+        end
+      end
     end
   end
-
-  resources :trip
-
-  devise_for :users, :controllers => {registrations: 'users/registrations', sessions: 'users/sessions'}
 
   get '/users/get_territories/:country', to: 'welcome#get_territories' 
   get '/users/get_cities/:country/:territory', to: 'welcome#get_cities' 
