b823a9faff0dd49bbfee0fee32bff9aa424d8e22
Yossi <yygold@gmail.com>
2020-08-20 22:16:16 -0400

    completed meal preferences, fixed dropdown issue in edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,16 @@ Rails.application.routes.draw do
   # get '/destinations/get_coordinates/:name', to: 'destinations#coordinates'
   devise_for :users, :controllers => {registrations: 'users/registrations', sessions: 'users/sessions'}
   resource :users do
+    resource :meal_preferences, shallow: true 
     resources :trips do
       resources :destinations do
-        resources :meal_preferences do 
           collection do
           get 'get_coordinates/:name', to: 'destinations#get_coordinates'
-          end
         end 
       end
     end
   end
   get '/users/get_territories/:country', to: 'welcome#get_territories' 
-  get '/users/get_cities/:country/:territory', to: 'welcome#get_cities' 
+  get '/users/get_cities/:country/:territory', to: 'welcome#get_cities'
 end
  
